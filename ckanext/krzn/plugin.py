3f7f3bc37824e33eee97b1000a80ddfbbc19da5a
Dominik Moritz <domoritz@gmail.com>
2013-07-23 16:53:58 +0200

    Initial import of plugin code

---
+++ ckanext/krzn/plugin.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class UnexpectedDateFormat(Exception):
+    pass
+
+
+def no_registering(context, data_dict):
+    return {'success': False, 'msg': toolkit._('''You cannot register for this
+        site. If you need an account, ask KRZN.''')}
+
+
+class KrznCustomizations(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer, inherit=True)
+    plugins.implements(plugins.IAuthFunctions, inherit=True)
+
+    def update_config(self, config):
+        here = os.path.dirname(__file__)
+        rootdir = os.path.dirname(os.path.dirname(here))
+
+        our_public_dir = os.path.join(rootdir, 'ckanext', 'krzn', 'theme',
+                'public')
+        template_dir = os.path.join(rootdir, 'ckanext', 'krzn', 'theme',
+                'templates')
+        config['extra_public_paths'] = ','.join([our_public_dir,
+                config.get('extra_public_paths', '')])
+        config['extra_template_paths'] = ','.join([template_dir,
+                config.get('extra_template_paths', '')])
+
+        toolkit.add_resource('theme/fanstatic_library', 'ckanext-krzn')
+
+    def get_auth_functions(self):
+        return {
+            'user_create': no_registering
+        }
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+version = '0.1'
+
+setuptools.setup(
+    name='ckanext-krzn',
+    version=version,
+    description="CKAN extension for KRZN",
+    long_description="""\
+    """,
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords='',
+    author='okfn',
+    author_email='dominik.moritz@okfn.org',
+    url='',
+    license='',
+    packages=setuptools.find_packages(
+        exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.krzn'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+        [ckan.plugins]
+    # Add plugins here
+    krzn=ckanext.krzn.plugin:KrznCustomizations
+
+    """,
+)
