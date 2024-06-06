0592ce472e3779b25dfa555752c201df945d104b
Mila Frerichs <mila.frerichs@gmail.com>
2024-06-06 16:08:45 +0200

    fix: remove deprecated url and replace with url_for

--- ckanext/krzn/plugin.py
+++ ckanext/krzn/plugin.py
@@ -8,6 +8,43 @@ def no_registering(context, data_dict):
     return {'success': False, 'msg': toolkit._('''You cannot register for this
         site. If you need an account, ask KRZN.''')}
 
+def get_current_context():
+    """
+    Get the current context (package and organization) safely.
+
+    Returns:
+        dict: A dictionary containing 'package' and 'organization' keys,
+              with their respective values if available, or None.
+    """
+    context = {
+        'package': None,
+        'organization': None
+    }
+
+    try:
+        if hasattr(toolkit.g, 'pkg_dict'):
+            context['package'] = toolkit.g.pkg_dict
+            if context['package'].get('organization'):
+                context['organization'] = toolkit.h.get_organization(context['package']['organization']['id'])
+
+        if not context['organization'] and hasattr(toolkit.g, 'group_dict'):
+            context['organization'] = toolkit.g.group_dict
+
+        if not context['organization']:
+            org_id = toolkit.request.view_args.get('id')
+            if org_id:
+                try:
+                    context['organization'] = toolkit.get_action('organization_show')(
+                        data_dict={'id': org_id}
+                    )
+                except toolkit.ObjectNotFound:
+                    pass
+
+    except (RuntimeError, AttributeError):
+        pass
+
+    return context
+
 
 class KrznCustomizations(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer, inherit=True)
@@ -34,4 +71,9 @@ class KrznCustomizations(plugins.SingletonPlugin):
         """ Do not allow registering through the API """
         return {
             'user_create': no_registering
+        }
+
+    def get_helpers(self):
+        return {
+            'get_current_context': get_current_context,
         }
